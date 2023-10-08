20ae6fe5ceed8362ac90974da4f2997f2fbcfc49
Sujith <88761608+Sujith1909@users.noreply.github.com>
2023-10-08 13:32:20 +0530

    Create errors.py

---
+++ errors.py
@@ -0,0 +1,18 @@
+# Example Python file with linting errors
+
+def add_numbers(a, b):
+    return a + b
+
+# Missing colon
+if True
+    print("This if statement has a syntax error")
+
+# Undefined variable
+result = multiply_numbers(3, 4)
+
+# Unused variable
+unused_variable = "I am not used"
+
+# Indentation error
+def function_with_indentation_error():
+return "This function has incorrect indentation"
